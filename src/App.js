c6470e5a9ea1de4a468f317d3fddfeb3c63c3fbc
Gabriel Delgado <gabriel.delgado@encora.com>
2023-03-15 16:58:18 -0400

    Added the antd library to the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import { Input, Button, Row, Col, message } from 'antd'
 
 function App() {
   return (
@@ -7,6 +8,14 @@ function App() {
       <input type="password" name="password" placeholder="Enter password" />
       <button onClick={() => alert('Login Successful!')}>Login</button>
       <hr />
+
+      <Row>
+        <Col span="6" offset="8">
+          <Input name="username" placeholder="Enter username" />
+          <Input.Password name="username" placeholder="Enter username" />
+          <Button type="primary" onClick={() => message.info('Login successful')}>Login</Button>
+        </Col>
+      </Row>
     </div>
   )
 }
